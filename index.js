e9807911545973fcd15898e4e8f088ce7f6fc114
Vivek Sondhiya <viveksondhiya555@gmail.com>
2024-03-07 13:08:54 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// const arr=[2,4,7,1,3,8,3]
+// let ans=arr.filter((i)=>{
+//     //console.log(i)
+//     return i>3
+// })
+
+// const fs=require('fs')
+// fs.writeFileSync("file.txt","this is my first file")
+// const path=fs.path("file.txt")
+// const con=fs.readSync()
+// console.log(con.toString)
+//
+//const http=require('http')
+//http.createServer((req,res)=>{
+//    res.write("<h1>Hello server</h1>")
+//    res.end();
+//}).listen(8000);
+
+const color=require('colors')
+console.log("hello".red)
+console.log("hello programmer")
