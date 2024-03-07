887ab395c464641ad5d9d89e6789bd63e2e09944
Vivek Sondhiya <viveksondhiya555@gmail.com>
2024-03-07 15:05:23 +0530

    further file - cmd,data,http

---
+++ data.js
@@ -0,0 +1,5 @@
+const data=[
+{ name: 'Vivek Sondhiya', email: 'viv@gmail.com' },
+{ name: 'Tony stark', email: 'tony@gmail.com' },
+{ name: 'Steav jobs', email: 'steav@gmail.com' }];
+module.exports=data;
